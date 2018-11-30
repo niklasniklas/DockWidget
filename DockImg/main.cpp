9ab25e85f8a5b76b8c67554441d8e2dc834fea26
niklasniklas <niclas@appleby.se>
2018-11-30 07:05:42 +0100

    urls

--- DockImg/main.cpp
+++ DockImg/main.cpp
@@ -9,3 +9,18 @@ int main(int argc, char *argv[])
 
     return a.exec();
 }
+
+
+/*
+https://www.qtcentre.org/threads/8723-Setting-an-icon-to-an-Item-in-a-QlistWidget?p=46559
+https://forum.qt.io/topic/1786/qlistwidget-reorder-with-drag-and-drop/11
+
+https://www.qtcentre.org/threads/37753-Drag-and-Drop-items-in-QListWidget
+https://wiki.qt.io/QList_Drag_and_Drop_Example
+https://stackoverflow.com/questions/8023983/how-to-show-icons-without-text-in-qlistwidget
+
+
+
+
+
+  */
